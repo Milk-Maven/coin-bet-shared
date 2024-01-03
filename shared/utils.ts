226ff125baf1ffeb740f055fd91bdbfcea13a89d
Milk-Maven <tylerf66@gmail.com>
2024-01-03 10:56:15 -0600

    update shared

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,7 +1,8 @@
 
 export const endpoints = {
   offeringCreate: 'offering/create',
-  offeringGet: 'offering/get'
+  offeringGet: 'offering/get',
+  startWeek: 'game/start'
 }
 
 export function checkCondition(condition: boolean, errorMessage: string): void {
@@ -12,14 +13,22 @@ export function checkCondition(condition: boolean, errorMessage: string): void {
 export const PUB_KEY: Readonly<string> = 'BC1YLgJ6FWVz9GKQwktGmgRQ7DDFZj65ZhyxTGiSGnCGcYX4Hhx2VaY'
 
 export type PartialWithRequiredFields<T, K extends keyof T> = Partial<T> & Pick<T, K>;
-
 export type OfferingExtraDateRequest = {
   endDate: string,
   totalOptions: string
-  isOffering: 'true'
+  postType: PostType.offering
+  creatorPublicKey: string
 };
 
 export type OfferingOptionsExtraDataRequest = {
   option: string
+};
+
+export enum PostType { offering = 'offering', startWeek = 'startWeek' }
+export type StartWeekRequest = {
+  description: string, // welcome to week three of the golden calf's trial. Until x/xx/xx/ users can submit an offering at gc.com. The top 3 choosen posts will be selected on x/xx/xx. Users can then vote on the option which they think is correct. Below the golden calf will post submissions from the app that users can directly vote on through their feed.
+  latestWeek: 'true'
+  currentWeek: string
+  postType: PostType.startWeek
 };
 
--- shared/validators.ts
+++ shared/validators.ts
@@ -1,6 +1,6 @@
-import * as zod from 'zod';
 
-import { z } from 'zod';
+import { TypeOf, z } from 'zod';
+import { PostType } from './utils.js';
 
 export const offeringCreateValidation = z.object({
   event_description: z.string().min(10).refine((data) => data !== '', { message: 'Event description is required' }),
@@ -8,7 +8,7 @@ export const offeringCreateValidation = z.object({
     z.string()
   ).refine((data) => {
     console.log(data)
-    return data[0].length && data[1].length
+    return data[0]?.length && data[1]?.length
   }, {
 
     message: 'At least two outcomes are required',
@@ -17,21 +17,36 @@ export const offeringCreateValidation = z.object({
     // Check if the value is a valid ISO string
     return /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3}Z?)?$/.test(data);
   }, { message: 'Invalid end date format' }),
+  publicKey: z.string().min(10)
 });
 
 // Example usage
 
-export const offeringGetValidation = zod.z.object({
-  PostHashHex: zod.z.string(),
-  OptionPostHashHex: zod.z.array(zod.z.string()),
-  PosterPublicKeyBase58Check: zod.z.string()
+export const offeringGetValidation = z.object({
+  PostHashHex: z.string(),
+  OptionPostHashHex: z.array(z.string()),
+  PosterPublicKeyBase58Check: z.string()
 
 });
 
-export type OfferringCreateRequest = zod.TypeOf<typeof offeringCreateValidation>;
-export type OfferingGetRequest = zod.TypeOf<typeof offeringGetValidation>;
+export type OfferringCreateRequest = TypeOf<typeof offeringCreateValidation>;
+export type OfferingGetRequest = TypeOf<typeof offeringGetValidation>;
 
 export const endpoints = {
   betNew: 'bet/new',
   betGet: 'bet/get'
 }
+
+export const startWeekValidation = z.object({
+  description: z.string(), // welcome to week three of the golden calf's trial. Until x/xx/xx/ users can submit an offering at gc.com. The top 3 choosen posts will be selected on x/xx/xx. Users can then vote on the option which they think is correct. Below the golden calf will post submissions from the app that users can directly vote on through their feed.
+  latestWeek: z.literal('true').or(z.literal('false')),
+  currentWeek: z.string(),
+  postType: z.literal(PostType.startWeek)
+
+});
+
+export type StartWeekRequest = TypeOf<typeof startWeekValidation>;
+
+
+
+
